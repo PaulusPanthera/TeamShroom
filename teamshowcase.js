cc24b6dad9a66af18566f848a052c18d272dc919
PaulusPanthera <pauluspanthera@gmail.com>
2025-06-23 18:37:52 +0200

    Update teamshowcase.js

--- teamshowcase.js
+++ teamshowcase.js
@@ -2,9 +2,8 @@ fetch('teamshowcase.json')
   .then(response => response.json())
   .then(data => {
     window.teamShowcase = data;
-    // If showcase.js is already loaded, you may want to trigger a re-render here.
+    // Optionally, trigger a re-render here if your site supports live updating
     if (window.setupShowcaseSearchAndSort && window.teamMembers) {
-      // e.g. re-render the gallery if needed
       window.setupShowcaseSearchAndSort(window.teamMembers, window.renderShowcaseGallery, "alphabetical");
     }
   });
