2cf447341c06181b1d87840c1cfea5c535f14639
PaulusPanthera <pauluspanthera@gmail.com>
2026-01-11 08:45:52 +0100

    Create donators.contract.js

---
+++ scripts/contracts/donators.contract.js
@@ -0,0 +1,6 @@
+export const donatorsSchema = [
+  { key: 'date', required: true },
+  { key: 'name', required: true },
+  { key: 'donation' },
+  { key: 'value', required: true, type: 'number' }
+];
